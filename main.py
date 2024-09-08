e950c59824d19897ea9251eed485883cd1a22898
Sajal Kuikel <sajalkuikel.edu@gmail.com>
2024-09-08 10:05:38 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title ("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+
+
+st.write("Hello ,let's learn how to build a streamlit app together")
+
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+st.write("Outside the form")
